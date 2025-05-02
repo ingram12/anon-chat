77495cffebeeabf6228a6b0148b036a5a3dae26f
atrubitsin <atrubitsin@promopult.ru>
2025-05-02 18:36:06 +0700

    Refactor UserStorage: change user map to store User values instead of pointers, update CreateUser, GetUser, and UpdateUser methods accordingly

--- internal/users/storage.go
+++ internal/users/storage.go
@@ -7,23 +7,23 @@ import (
 
 type UserStorage struct {
 	mu    sync.RWMutex
-	users map[[36]byte]*User
+	users map[[36]byte]User
 }
 
 func NewUserStorage() *UserStorage {
 	return &UserStorage{
-		users: make(map[[36]byte]*User),
+		users: make(map[[36]byte]User),
 	}
 }
 
-func (s *UserStorage) CreateUser(userID string) *User {
+func (s *UserStorage) CreateUser(userID string) User {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	var idBytes [36]byte
 	copy(idBytes[:], userID)
 
-	user := &User{
+	user := User{
 		ID:           idBytes,
 		CreatedAt:    time.Now(),
 		IsRegistered: false,
@@ -33,7 +33,7 @@ func (s *UserStorage) CreateUser(userID string) *User {
 	return user
 }
 
-func (s *UserStorage) GetUser(userID string) (*User, bool) {
+func (s *UserStorage) GetUser(userID string) (User, bool) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
@@ -55,7 +55,7 @@ func (s *UserStorage) IsUserExist(userID string) bool {
 	return exists
 }
 
-func (s *UserStorage) UpdateUser(user *User) {
+func (s *UserStorage) UpdateUser(user User) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
