c5a01dc150f807843ea9e179b57a30ff06646357
atrubitsin <atrubitsin@promopult.ru>
2025-05-05 01:42:08 +0700

    fix: update swagger route and improve OAPI middleware configuration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,10 @@ import (
 	"anon-chat/internal/config"
 	"anon-chat/internal/handlers"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	middleware "github.com/labstack/echo/v4/middleware"
@@ -24,17 +28,6 @@ func main() {
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 	}))
 
-	// Use OAPI middleware for request validation based on the Swagger spec.
-	swagger, err := api.GetSwagger()
-	if err != nil {
-		panic("Failed to load swagger.json")
-	}
-	e.Use(middlewareValidator.OapiRequestValidator(swagger))
-
-	configuration := config.NewConfig()
-	userService := handlers.NewUserService(configuration)
-	api.RegisterHandlers(e, userService)
-
 	// Add route to serve swagger.json
 	e.GET("/swagger.json", func(c echo.Context) error {
 		swagger, err := api.GetSwagger()
@@ -43,6 +36,36 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, swagger)
 	})
+
+	// Use OAPI middleware for request validation based on the Swagger spec.
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		panic("Failed to load swagger.json")
+	}
+	e.Use(middlewareValidator.OapiRequestValidatorWithOptions(swagger, &middlewareValidator.Options{
+		Skipper: func(c echo.Context) bool {
+			return !strings.HasPrefix(c.Path(), "/api/")
+		},
+	}))
+
+	configuration := config.NewConfig()
+	userService := handlers.NewUserService(configuration)
+	api.RegisterHandlers(e, userService)
+
+	if os.Getenv("APP_ENV") == "dev" {
+		// Прокси dev-сервера Vite через Go
+		target, _ := url.Parse("http://localhost:5173")
+		proxy := httputil.NewSingleHostReverseProxy(target)
+
+		// Всё кроме /api отдаём через прокси
+		e.GET("/*", echo.WrapHandler(proxy))
+	} else {
+		// В продакшне отдаем собранный фронт
+		e.Static("/", "frontend")
+		e.GET("/", func(c echo.Context) error {
+			return c.File("frontend/index.html")
+		})
+	}
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
